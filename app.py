28a41f7cc463a93a86d92b7633dceb35567a4d51
Santiago <santicepeda03@gmail.com>
2021-12-15 07:08:43 -0300

    tengo sueño

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app = Flask(__name__)
 # TODO agregar lista de máquinas para hacer
 # TODO agregar suma de ganancias
 # TODO agregar lista de gastos
-
+# TODO agregar lista de cliente con deudas y máquinas
 
 @app.route('/')
 def index():
@@ -48,45 +48,39 @@ def add_existing_machine_index():
     ID = str(request.args.get('id'))
     with open('data.json') as file:
         raw_data = json.load(file)
-        history = raw_data[0][ID]
-        data = {
-            'machine_data': {
-                'client': raw_data[0][ID][-1]['client'],
-                'machine': raw_data[0][ID][-1]['machine_model'],
-                'ID': raw_data[0][ID][-1]['ID']
-            },
-            'problems_history': history
-        }
+    history = raw_data[0][ID]['issues']
+    data = {
+        'machine_data': {
+            'client_data': raw_data[1][str(raw_data[0][ID]['client_ID'])],
+            'machine': raw_data[0][ID]['machine_model'],
+            'ID': raw_data[0][ID]['ID']
+        },
+        'problems_history': history
+    }
 
-        return render_template('add_existing_machine.html', data= data)
+    return render_template('add_existing_machine.html', data= data)
 
 @app.route('/addexistingmachine', methods=['GET', 'POST'])
 def add_existing_machine():
     ID = str(request.args.get('id'))
     with open('data.json') as file:
-        data = json.load(file)
-    machine_model = data[0][ID][-1]['machine_model']
-    client = data[0][ID][-1]['client']
-    client_phone = data[0][ID][-1]['client_phone']
+        new_data = json.load(file)
     problems = request.form.get('problems')
     new_issue = {
-        'ID': ID,
-        'client': client,
-        'client_phone': client_phone,
-        'machine_model': machine_model,
         'problems': problems,
-        'status': 'en espera',
         'workforce': {},
         'spare_parts': {},
         'total_price': 0,
         'arrive_date': ctime(), # agrega la fecha en la que se agregó
-        'fixing_date': ''
+        'fixing_date': '',
+        'delivery_date': ''
     }
-    data[0][ID].append(new_issue)
-
+    
+    new_data[0][ID]['issues'].append(new_issue)
+    new_data[0][ID]['status'] = 'Pendiente'
+    
     with open('data.json', 'w') as file:
-        json.dump(data, file, indent=4)
-    data = data[0][ID][-1]
+        json.dump(new_data, file, indent=4)
     
     return redirect(f'http://192.168.1.69:5000/machine?id={ID}')
 
@@ -96,12 +90,10 @@ def show_machine():
     ID = str(request.args.get('id'))
     with open('data.json') as file:
         raw_data = json.load(file)
-        try:
-            page = int(request.args.get('page'))
-        except:
-            page = len(raw_data[0][ID]) - 1
-        data = raw_data[0][ID][page]
-
+    client_ID = str(raw_data[0][ID]['client_ID'])
+    machine_data = raw_data[0][ID]
+    client_data = raw_data[1][str(raw_data[0][ID]['client_ID'])]
+    data = {'m': machine_data['issues'][-1], 'c': client_data, 'a': machine_data}
     if request.method == 'POST':
         service = request.form.get("service")
         service_price = request.form.get("service_price")
@@ -109,68 +101,91 @@ def show_machine():
         spare_part_price = request.form.get("spare_part_price")
         # agrega repuestos y mano de obra
         if service_price != "" and service_price != None:
-            raw_data[0][ID][-1]['workforce'][service] = int(service_price)
+            raw_data[0][ID]['issues'][-1]['workforce'][service] = int(service_price)
         if spare_part_price != "" and spare_part_price != None:
-            raw_data[0][ID][-1]['spare_parts'][spare_part] = int(spare_part_price)
+            raw_data[0][ID]['issues'][-1]['spare_parts'][spare_part] = int(spare_part_price)
         # cambia el estado a arreglado y calcula el precio total
         if request.form.get('fixed') == 'fixed':
-            if raw_data[0][ID][-1]['status'] == 'en espera':
-                raw_data[0][ID][-1]['status'] = 'Para entregar'
+            if raw_data[0][ID]['status'] == 'Pendiente':
+                raw_data[0][ID]['status'] = 'Para entregar'
                 total_price = 0
-                for service, price in raw_data[0][ID][-1]['workforce'].items():
+                for service, price in raw_data[0][ID]['issues'][-1]['workforce'].items():
                     total_price += price
-                for spare_part, price in raw_data[0][ID][-1]['spare_parts'].items():
+                for spare_part, price in raw_data[0][ID]['issues'][-1]['spare_parts'].items():
                     total_price += price
-                raw_data[0][ID][-1]['total_price'] = total_price
-                raw_data[0][ID][-1]['fixing_date'] = ctime()
+                raw_data[0][ID]['issues'][-1]['total_price'] = total_price
+                raw_data[0][ID]['issues'][-1]['fixing_date'] = ctime()
+
+                raw_data[1][client_ID]['debt'] += raw_data[0][ID]['issues'][-1]['total_price']
+
         # cambia el estado a entregada
-        if raw_data[0][ID][-1]['status'] == 'Para entregar':
+        if raw_data[0][ID]['status'] == 'Para entregar':
             if request.form.get('delivered') == 'delivered':
-                raw_data[0][ID][-1]['status'] = 'Entregada'
-                raw_data[0][ID][-1]['delivery_date'] = ctime()
+                raw_data[0][ID]['status'] = 'Entregada'
+                raw_data[0][ID]['issues'][-1]['delivery_date'] = ctime()
+                pay = request.form.get('pay')
+                raw_data[1][client_ID]['debt'] -= int(pay)
+    with open('data.json', 'w') as file:
+        json.dump(raw_data, file, indent=4)
         if request.form.get('New issue') == 'New issue':
             return redirect(f'http://192.168.1.69:5000/addexistingmachine?id={ID}')
-            
-        with open('data.json', 'w') as file:
-            json.dump(raw_data, file, indent=4)
+
 
     return render_template('machines.html', data=data)
 
 
 def add_machine(client, client_phone, machine_model, problems):
     '''adds a row to the excel file with the data of a machine
-    row: 1=ID, 2=client, 3=client_phone, 4=machine, 5=problems, 6=status'''
+    row: 1=client, 2=client_phone, 3=machine, 4=problems'''
     with open('data.json') as file:
         data = json.load(file)
     # crea un nuevo ID de máquina
     ID = int(len(data[0])+101)
+    # agrega la máquina al cliente y define el client_ID
+    if client in data[2]['clients']:
+        for client_data in data[1]:
+            if client_data['client'] == client:
+                client_ID = data[1]['client_ID']
+                data[1][client_ID]['machines'].append(ID)
+    # agrega el cliente con la máquina en el caso de que no esté
+    else:
+        client_ID = len(data[1]) + 101
+        client_data = {
+            "client_ID": client_ID,
+            "client": client,
+            "client_phone": client_phone,
+            "machines": [ID],
+            "debt": 0
+        }
+        data[1][client_ID] = client_data
+        data[2]['clients'].append(client)
     # crea un diccionario con todos los datos para agregarlo al archivo .json
     to_json = {
         'ID': ID,
-        'client': client,
-        'client_phone': client_phone,
+        'client_ID': client_ID,
+        'status': 'Pendiente',
         'machine_model': machine_model,
-        'problems': problems,
-        'status': 'en espera',
-        'workforce': {},
-        'spare_parts': {},
-        'total_price': 0,
-        'arrive_date': ctime(), # agrega la fecha en la que se agregó
-        'fixing_date': '',
-        'deivery_date': ''
+        'issues': [
+            {
+                'problems': problems,
+                'workforce': {},
+                'spare_parts': {},
+                'total_price': 0,
+                'arrive_date': ctime(), # agrega la fecha en la que se agregó
+                'fixing_date': '',
+                'deivery_date': ''
+            }
+        ]
     }
-    # pide confirmación para continuar
-    # abre la data anterior
-    machines_dict = data[0]
     # en el caso de que la máquina ya haya estado se agrega el problema a la lista
-    if ID in machines_dict:
-        machines_dict[ID].append(to_json)
+    if ID in data[0]:
+        data[0][ID]['issues'].append(to_json['issues'])
         # agrega el elemento al diccionario en forma de [ID] = data
     else:
-        machines_dict[ID] = [to_json]
+        data[0][ID] = to_json
     # vuelve a escribir la data actualizada en el archivo .json
     with open('data.json', 'w') as file:
-        json.dump([machines_dict], file, indent=4)
+        json.dump(data, file, indent=4)
     # envía la ID al HTML
     return ID
 
